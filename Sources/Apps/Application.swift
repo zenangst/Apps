c8a16519b00622a9024ecce34a2be6dda54940a1
Christoffer Winterkvist <christoffer@winterkvist.com>
2022-03-22 09:19:03 +0100

    Refactor Application to use `bundleIdentifier` as `id`

--- Sources/Apps/Application.swift
+++ Sources/Apps/Application.swift
@@ -6,19 +6,17 @@ public struct Application: Identifiable, Codable, Hashable, Sendable {
     public var isElectron: Bool = false
   }
 
-  public let id: String
+  public var id: String { bundleIdentifier }
   public let bundleIdentifier: String
   public let bundleName: String
   public let path: String
   public var metadata: Metadata = Metadata()
   public private(set) var displayName: String = ""
 
-  public init(id: String = UUID().uuidString,
-              bundleIdentifier: String,
+  public init(bundleIdentifier: String,
               bundleName: String,
               displayName: String? = nil,
               path: String) {
-    self.id = id
     self.bundleIdentifier = bundleIdentifier
     self.bundleName = bundleName
     self.path = path
@@ -26,7 +24,6 @@ public struct Application: Identifiable, Codable, Hashable, Sendable {
   }
 
   enum CodingKeys: String, CodingKey {
-    case id
     case bundleIdentifier
     case bundleName
     case path
@@ -35,10 +32,15 @@ public struct Application: Identifiable, Codable, Hashable, Sendable {
   public init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
 
-    self.id = try container.decodeIfPresent(String.self, forKey: .id) ?? UUID().uuidString
     self.bundleIdentifier = try container.decode(String.self, forKey: .bundleIdentifier)
     self.bundleName = try container.decode(String.self, forKey: .bundleName)
     self.path = try container.decode(String.self, forKey: .path)
     self.displayName = FileManager().displayName(atPath: path)
+  }
+
+  public static func ==(lhs: Application, rhs: Application) -> Bool {
+    lhs.bundleIdentifier == rhs.bundleIdentifier &&
+    lhs.bundleName == rhs.bundleName &&
+    lhs.path == rhs.path
   }
 }
